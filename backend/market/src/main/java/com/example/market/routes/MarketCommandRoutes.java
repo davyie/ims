f5b2149c440ce47d249a3acb4e0544200c333433
David Yu <yu.davidcheng@gmail.com>
2025-06-16 21:57:34 +0200

    Add decrement functionality to market. It doesn't decrement the warehouse

--- backend/market/src/main/java/com/example/market/routes/MarketCommandRoutes.java
+++ backend/market/src/main/java/com/example/market/routes/MarketCommandRoutes.java
@@ -2,6 +2,7 @@ package com.example.market.routes;
 
 import com.example.market.commands.AddMarketItemToMarketCommand;
 import com.example.market.commands.CreateMarketCommand;
+import com.example.market.commands.DecrementMarketItemQuantityCommand;
 import com.example.market.commands.DeleteMarketItemCommand;
 import com.example.market.domain.Market;
 import com.example.market.domain.MarketDTO;
@@ -10,6 +11,7 @@ import com.example.market.domain.MarketItemQuantityDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,14 +21,17 @@ public class MarketCommandRoutes {
     private CreateMarketCommand cmc;
     private AddMarketItemToMarketCommand amimc;
     private DeleteMarketItemCommand dmic;
+    private DecrementMarketItemQuantityCommand dmiqc;
 
     @Autowired
     public MarketCommandRoutes(CreateMarketCommand cmc,
                                AddMarketItemToMarketCommand addMarketItemToMarketCommand,
-                               DeleteMarketItemCommand dmic) {
+                               DeleteMarketItemCommand dmic,
+                               DecrementMarketItemQuantityCommand dmiqc) {
         this.cmc = cmc;
         this.amimc = addMarketItemToMarketCommand;
         this.dmic = dmic;
+        this.dmiqc = dmiqc;
     }
 
     @PostMapping("/create")
@@ -43,5 +48,10 @@ public class MarketCommandRoutes {
     @DeleteMapping("/delete/item")
     public ResponseEntity<Market> deleteItems(@RequestBody MarketItemQuantityDTO dto) {
         return new ResponseEntity<>(this.dmic.execute(dto.getWarehouseId(), dto.getMarketName(), dto.getItemId()), HttpStatus.OK);
+    }
+
+    @PutMapping("/decrement/item")
+    public ResponseEntity<Market> decrementItem(@RequestBody MarketItemQuantityDTO dto) {
+        return new ResponseEntity<>(this.dmiqc.execute(dto.getMarketName(), dto.getItemId(), dto.getQuantity()), HttpStatus.ACCEPTED);
     }
 }
